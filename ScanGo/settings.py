4150d92eb77fbb797a117ff5ce54c45b44a814ba
shiv2321 <pujarishiv1718@gmail.com>
2025-11-17 18:35:35 +0530

    fixed settings.py for calling backend

--- ScanGo/settings.py
+++ ScanGo/settings.py
@@ -29,13 +29,7 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "scango_web",
-    "scango_nginx",
-    "scango.giize.com",
-    "www.scango.giize.com",
-
+    "*",
 ]
 
 
