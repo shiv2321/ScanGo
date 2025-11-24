1bf8c04bbe12ba5a9e3d6b627a670919452040ea
shiv2321 <pujarishiv1718@gmail.com>
2025-11-24 23:38:58 +0530

    fixed api.js for production

--- scango-frontend/src/api.js
+++ scango-frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: 
+    process.env.NODE_ENV === "production"
+    ? "https://scango.giize.com/api"
+    : "http://127.0.0.1:8000/api",
 });
 
 export default api;
